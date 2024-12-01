ccb05c063bdcb6ce0bbf29511c76772024ac686f
SalahShehada <salah.shehade12@gmail.com>
2024-12-01 15:05:14 +0200

    ChatScreen Basic

--- lib/Screen/chatscreen.dart
+++ lib/Screen/chatscreen.dart
@@ -7,7 +7,14 @@ class ChatScreen extends StatefulWidget {
   State<ChatScreen> createState() => _ChatScreenState();
 }
 
-class _ChatScreenState extends State<ChatScreen> {
+class _ChatScreenState extends State<ChatScreen> with SingleTickerProviderStateMixin {
+  late TabController _controller;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = TabController(length: 4, vsync: this, initialIndex: 1);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +24,30 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(icon: Icon(Icons.search), onPressed: () {},),
           IconButton(icon: Icon(Icons.more_vert), onPressed: () {},)
         ],
+        bottom: TabBar(
+          controller: _controller,
+          tabs: [
+            Tab(
+              icon: Icon(Icons.camera_alt),
+            ),
+            Tab(
+              text: "CHATS",
+            ),
+            Tab(
+              text: "STATUS",
+            ),
+            Tab(text: "CALLS",),
+          ],
+        ),
       ),
+      body: TabBarView(
+        controller: _controller,
+        children: [
+        Text("Camera"),
+        Text("Chats"),
+        Text("Status"),
+        Text("Calls"),
+      ],),
     );
   }
 }
