804b35edc48ee2c84681d9f77ef59ea6f2f56c24
SalahShehada <salah.shehade12@gmail.com>
2024-12-01 12:47:56 +0200

    some checking for chatscreen file

--- lib/Screen/chatscreen.dart
+++ lib/Screen/chatscreen.dart
@@ -10,6 +10,14 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("whatsapp clone"),
+        actions: [
+          IconButton(icon: Icon(Icons.search), onPressed: () {},),
+          IconButton(icon: Icon(Icons.more_vert), onPressed: () {},)
+        ],
+      ),
+    );
   }
 }
