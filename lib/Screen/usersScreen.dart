8a50cc90f44d86d24951e11196165bc89abf5871
AbdallahKhayat <abdullahabdkhayat@gmail.com>
2024-12-15 16:55:44 +0200

    added search for Users

--- lib/Screen/usersScreen.dart
+++ lib/Screen/usersScreen.dart
@@ -13,11 +13,42 @@ class UsersScreen extends StatefulWidget {
 class _UsersScreenState extends State<UsersScreen> {
   List<dynamic> users = []; // List to store fetched users
   NetworkHandler networkHandler = NetworkHandler();
-
+  TextEditingController _searchController = TextEditingController();
   @override
   void initState() {
     super.initState();
     fetchUsers(); // Fetch users on initialization
+  }
+
+  Future<void> searchUsers(String query) async {
+    if (query.isEmpty) {
+      fetchUsers(); // Reset to full list if search is empty
+      return;
+    }
+
+
+      var response = await networkHandler.get("/user/search/$query");
+
+    if (response != null) {
+      if (response is List) {
+        // If response is already a list, assign it directly
+        setState(() {
+          users = response;
+        });
+      } else if (response['data'] != null && response['data'] is List) {
+        // If response contains 'data' key and it's a list, assign the data
+        setState(() {
+          users = response['data'];
+        });
+      }
+    } else {
+      debugPrint("No users found");
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("No users found")),
+      );
+    }
+
+
   }
 
   // Function to fetch users using NetworkHandler
@@ -120,232 +151,145 @@ class _UsersScreenState extends State<UsersScreen> {
     }
   }
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(""),
+        title: SizedBox(
+          height: 40,
+          child: TextField(
+            controller: _searchController,
+            onChanged: (query) => searchUsers(query),
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              hintText: "Search Users...",
+              prefixIcon: const Icon(Icons.search, color: Colors.grey),
+              contentPadding: const EdgeInsets.symmetric(vertical: 5),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide.none,
+              ),
+            ),
+            style: const TextStyle(fontSize: 14),
+          ),
+        ),
+        backgroundColor: Colors.teal, // App bar color
       ),
       body: users.isEmpty
           ? const Center(child: CircularProgressIndicator())
           : kIsWeb
-              ? Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 4, // Number of items per row
-                      crossAxisSpacing: 10.0, // Spacing between columns
-                      mainAxisSpacing: 10.0, // Spacing between rows
-                      childAspectRatio: 2.0, // Adjust the aspect ratio of items
-                    ),
-                    itemCount: users.length,
-                    itemBuilder: (context, index) {
-                      var user = users[index];
-                      bool isBanned = user['isBanned'] == true;
+          ? Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4, // Number of items per row
+            crossAxisSpacing: 10.0, // Spacing between columns
+            mainAxisSpacing: 10.0, // Spacing between rows
+            childAspectRatio: 2, // Adjust the aspect ratio of items
+          ),
+          itemCount: users.length,
+          itemBuilder: (context, index) => _buildUserCard(users[index]),
+        ),
+      )
+          : ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) => _buildUserCard(users[index]),
+      ),
+    );
+  }
 
-                      return Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(12.0),
-                          border:
-                              Border.all(color: Colors.grey.shade300, width: 1),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.shade200,
-                              blurRadius: 8.0,
-                              spreadRadius: 2.0,
-                              offset: const Offset(0, 4),
-                            ),
-                          ],
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Username: ${user['username']}",
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black87,
-                                ),
-                              ),
-                              const SizedBox(height: 4),
-                              Text(
-                                "Email: ${user['email']}",
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.black54,
-                                ),
-                              ),
-                              const SizedBox(height: 4),
-                              Text(
-                                "Role: ${user['role']}",
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w600,
-                                  color: user['role'] == "customer"
-                                      ? Colors.blue
-                                      : user['role'] == "admin"
-                                          ? Colors.red
-                                          : Colors.green,
-                                ),
-                              ),
-                              const SizedBox(height: 4),
-                              Text(
-                                isBanned ? "Status: Banned" : "Status: Active",
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: isBanned ? Colors.red : Colors.green,
-                                ),
-                              ),
-                              const SizedBox(height: 16),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  TextButton(
-                                    onPressed: () =>
-                                        toggleBanStatus(user['email']),
-                                    child: Text(
-                                      isBanned ? "Unban" : "Ban",
-                                      style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                        color: isBanned
-                                            ? Colors.green
-                                            : Colors.red,
-                                      ),
-                                    ),
-                                    // style: TextButton.styleFrom(
-                                    //   foregroundColor:
-                                    //       isBanned ? Colors.green : Colors.red,
-                                    // ),
-                                  ),
-                                  const SizedBox(width: 10),
-                                  TextButton(
-                                    onPressed: () => toggleUserRole(
-                                        user['email'], user['role']),
-                                    child: Text(user['role'] == "customer"
-                                        ? "Unpromote"
-                                        : "Promote",style:
-                                      TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.green,
-                                      ),),
-                                    // style: TextButton.styleFrom(
-                                    //   foregroundColor: Colors.green,
-                                    // ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    },
+  Widget _buildUserCard(user) {
+    bool isBanned = user['isBanned'] == true;
+
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12.0),
+        border: Border.all(color: Colors.grey.shade300, width: 1),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade200,
+            blurRadius: 8.0,
+            spreadRadius: 2.0,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Username: ${user['username']}",
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+            const SizedBox(height: 4),
+            Text(
+              "Email: ${user['email']}",
+              style: const TextStyle(fontSize: 16, color: Colors.black54),
+            ),
+            const SizedBox(height: 4),
+            Text(
+              "Role: ${user['role']}",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: user['role'] == "customer"
+                    ? Colors.blue
+                    : user['role'] == "admin"
+                    ? Colors.red
+                    : Colors.green,
+              ),
+            ),
+            const SizedBox(height: 4),
+            Text(
+              isBanned ? "Status: Banned" : "Status: Active",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: isBanned ? Colors.red : Colors.green,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () => toggleBanStatus(user['email']),
+                  child: Text(
+                    isBanned ? "Unban" : "Ban",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: isBanned ? Colors.green : Colors.red,
+                    ),
                   ),
-                )
-              : ListView.builder(
-                  itemCount: users.length,
-                  itemBuilder: (context, index) {
-                    var user = users[index];
-                    bool isBanned = user['isBanned'] == true;
-                    return Container(
-                      margin: const EdgeInsets.symmetric(
-                          vertical: 8.0, horizontal: 16.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(12.0),
-                        border:
-                            Border.all(color: Colors.grey.shade300, width: 1),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.shade200,
-                            blurRadius: 8.0,
-                            spreadRadius: 2.0,
-                            offset: const Offset(0, 4),
-                          ),
-                        ],
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Username: ${user['username']}",
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black87,
-                              ),
-                            ),
-                            const SizedBox(height: 4),
-                            Text(
-                              "Email: ${user['email']}",
-                              style: const TextStyle(
-                                fontSize: 14,
-                                color: Colors.black54,
-                              ),
-                            ),
-                            const SizedBox(height: 4),
-                            Text(
-                              "Role: ${user['role']}",
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w600,
-                                color: user['role'] == "customer"
-                                    ? Colors.blue
-                                    : user['role'] == "admin"
-                                        ? Colors.red
-                                        : Colors.green,
-                              ),
-                            ),
-                            const SizedBox(height: 4),
-                            Text(
-                              isBanned ? "Status: Banned" : "Status: Active",
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                                color: isBanned ? Colors.red : Colors.green,
-                              ),
-                            ),
-                            const SizedBox(height: 16),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                TextButton(
-                                  onPressed: () =>
-                                      toggleBanStatus(user['email']),
-                                  child: Text(isBanned ? "Unban" : "Ban"),
-                                  style: TextButton.styleFrom(
-                                    foregroundColor:
-                                        isBanned ? Colors.green : Colors.red,
-                                  ),
-                                ),
-                                const SizedBox(width: 10),
-                                TextButton(
-                                  onPressed: () => toggleUserRole(
-                                      user['email'], user['role']),
-                                  child: Text(user['role'] == "customer"
-                                      ? "Unpromote"
-                                      : "Promote"),
-                                  style: TextButton.styleFrom(
-                                    foregroundColor: Colors.green,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
                 ),
+                const SizedBox(width: 10),
+                TextButton(
+                  onPressed: () => toggleUserRole(user['email'], user['role']),
+                  child: Text(
+                    user['role'] == "customer" ? "Unpromote" : "Promote",
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
